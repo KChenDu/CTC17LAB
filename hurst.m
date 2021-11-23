7f0a76c0743c3080ca785b26c097436d82743920
KChenDu <kelvenchendu@gmail.com>
2021-11-23 02:04:32 -0300

    Hurst done

--- hurst.m
+++ hurst.m
@@ -2,39 +2,47 @@ clear
 clc
 
 data = getData('PETR4.SA.csv');
-len = length(data);
-N = fix(len / 10);
-x = zeros(10, 1);
 
-for i = 1 : 9
-    m = mean(data(1 : N));
-    diff = data(1 : N) - m;
+len = length(data);
+y = [];
+x = [];
+N = 8;
+
+while N < len
+    x = [x; N];
+    prev = 1;
+    RSk = [];
+    i = N;
+    while i < len
+        RSk = [RSk getRS(data(prev : i))];
+        prev = i + 1;
+        i = N + i;
+    end
+    RSk = [RSk getRS(data(prev : end))];
+    y = [y; mean(RSk)];
+    N = 2 * N;
+end
+x = [x; len];
+y = [y; getRS(data)];
+
+x = log2(x);
+y = log2(y);
+
+hurst_exp = regress(y, x);
+
+function RS = getRS(data)
+    m = mean(data);
+    diff = data - m;
     Vk  = diff(1);
-    RS = zeros(N, 1);
+    N = length(data);
+    % RS = zeros(N, 1);
     V = zeros(N, 1);
     V(1) = Vk;
     for k = 2 : N
         Vk = Vk + diff(k);
         V(k) = Vk;
-        RS(k) = (max(V(1 : k)) - min(V(1 : k))) / std(data(1 : k));
+        % RS(k) = (max(V(1 : k)) - min(V(1 : k))) / std(data(1 : k));
     end
-    x(i) = mean(RS);
-    data(1 : N) = [];
+    RS = (max(V) - min(V)) / std(data);
+    %RS = mean(RS);
 end
-
-m = mean(data(1 : end));
-diff = data(1 : end) - m;
-Vk  = diff(1);
-len = length(data);
-RS = zeros(len, 1);
-V = zeros(len, 1);
-V(1) = Vk;
-for k = 2 : len
-    Vk = Vk + diff(k);
-    V(k) = Vk;
-    RS(k) = (max(V(1 : k)) - min(V(1 : k))) / std(data(1 : k));
-end
-x(10) = mean(RS);
-
-% y = c * k^h;
-% f = fit(x, y, 'exp1');
